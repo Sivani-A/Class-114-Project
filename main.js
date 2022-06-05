0e5618a008d3f52b7a4103e9fbea05a3f18f144e
Sivani-A <72449124+Sivani-A@users.noreply.github.com>
2022-06-05 11:56:32 +0545

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preload(){
+
+}
+
+function draw(){
+
+}
+
+function setup(){
+    canvas= createCanvas(350,350)
+    canvas.center()
+}
+
+function takeSnapshot(){
+    Save('myImageFiltered.png')
+}
